2481a6fc1e4285b9ab0cd66ca8e6c7efd701a60c
naomitheqa <nobenjamin19@gmail.com>
2025-04-16 14:28:19 -0500

    feat: init

---
+++ tcr-fe/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { TextBoxComponent as EjsTextbox } from "@syncfusion/ej2-vue-inputs";
+import { ButtonComponent as EjsButton } from "@syncfusion/ej2-vue-buttons";
+</script>
+
+<template>
+
+  <div>
+    <form class="flex flex-col items-center gap-6 p-7">
+      <input class="e-input" type="text" placeholder="Enter something" />
+    </form>
+  </div>
+
+</template>
+
+<style scoped>
+  @import "../node_modules/@syncfusion/ej2-base/styles/tailwind.css";
+  @import "../node_modules/@syncfusion/ej2-vue-inputs/styles/tailwind.css";
+</style>
---
+++ tcr-fe/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense("Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXtfcXVSRGBcWUN2WEZWYUA=");
+
+createApp(App).mount('#app')
